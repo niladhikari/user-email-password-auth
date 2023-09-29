f178e3bcf69c41b5ee95bf1b760c60826088bcfa
niladhikari <niladhikari445@gmail.com>
2023-09-29 15:16:23 +0600

    add all the work email varification,,,password,,,login functionality

--- src/Components/HeroRegister/HeroRegister.jsx
+++ src/Components/HeroRegister/HeroRegister.jsx
@@ -1,24 +1,47 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../Firebase/firebase.config";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const HeroRegister = () => {
-   
-    const handleRegister = (e)=>{
-        e.preventDefault();
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        console.log(email,password);
+  const [registerError, setRegisterError] = useState("");
+  const [registerSuccess, setRegisterSuccess] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const accepted = e.target.terms.checked;
+    console.log(email, password);
 
-        //create user
+    // reset the error
+    setRegisterError("");
+    setRegisterSuccess("");
 
-        createUserWithEmailAndPassword(auth,email,password)
-        .then(result=>{
-          console.log(result.user);
-        })
-        .catch((error) => {
-           console.log(error);
-        });
+    //password validation
+    if (password.length < 6) {
+      setRegisterError("Password should be least 6 Characters or longer");
+      return;
+    } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9]/.test(password)) {
+      setRegisterError(
+        "Your password should have at least one upper case,lower case characters and number."
+      );
+      return;
     }
+    else if (!accepted) {
+      setRegisterError('Please Accept our Terms and Conditions')
+    }
+
+    //create user
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        setRegisterSuccess("Your Email Register Successfully", result);
+      })
+      .catch((error) => {
+        console.log(error);
+        setRegisterError(error.message);
+      });
+  };
 
   return (
     <div>
@@ -46,23 +69,80 @@ const HeroRegister = () => {
                   <label className="label">
                     <span className="label-text">Password</span>
                   </label>
-                  <input
-                    type="password"
-                    placeholder="password"
-                    className="input input-bordered"
-                    name="password"
-                    required
-                  />
+                  <div className="flex gap-1 items-center">
+                    <div className="relative">
+                      <input
+                        type={showPassword ? "text" : "password"}
+                        placeholder="password"
+                        className="input input-bordered"
+                        name="password"
+                        required
+                      />
+                    </div>
+                    <span
+                      onClick={() => setShowPassword(!showPassword)}
+                      className="absolute ml-48"
+                    >
+                      {showPassword ? (
+                        <FaEyeSlash></FaEyeSlash>
+                      ) : (
+                        <FaEye></FaEye>
+                      )}
+                    </span>
+                  </div>
                   <label className="label">
                     <a href="#" className="label-text-alt link link-hover">
                       Forgot password?
                     </a>
                   </label>
                 </div>
+                <div className="-ml-2.5">
+                  <div className="inline-flex items-center">
+                    <label
+                      className="relative flex cursor-pointer items-center rounded-full p-3"
+                      htmlFor="terms"
+                      data-ripple-dark="true"
+                    >
+                      <input
+                        type="checkbox"
+                        name="terms"
+                        id="terms"
+                        className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-pink-500 checked:bg-pink-500 checked:before:bg-pink-500 hover:before:opacity-10"
+                      />
+                      <span className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-3.5 w-3.5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          stroke="currentColor"
+                        >
+                          <path d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"></path>
+                        </svg>
+                      </span>
+                    </label>
+                    <label
+                      className="mt-px cursor-pointer select-none font-light text-gray-700"
+                      htmlFor="checkbox"
+                    >
+                      Accept our <a href="">Terms and Conditions</a>
+                    </label>
+                  </div>
+                </div>
                 <div className="form-control mt-6">
                   <button className="btn btn-primary">Register</button>
                 </div>
               </form>
+              {registerError && (
+                <p className="text-red-600 text-sm font-semibold pb-4 text-center">
+                  {registerError}
+                </p>
+              )}
+              {registerSuccess && (
+                <p className="text-green-600 text-sm font-semibold pb-4 text-center">
+                  {registerSuccess}
+                </p>
+              )}
             </div>
           </div>
         </div>
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,39 +1,68 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  updateProfile,
+} from "firebase/auth";
 import auth from "../../Firebase/firebase.config";
 import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  const [registerError,setRegisterError] = useState('')
-  const [registerSuccess,setRegisterSuccess] = useState('')
+  const [registerError, setRegisterError] = useState("");
+  const [registerSuccess, setRegisterSuccess] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleRegister = (e) => {
     e.preventDefault();
     const name = e.target.name.value;
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(name,email, password);
-
+    const accepted = e.target.terms.checked;
+    console.log(name, email, password, accepted);
 
     // reset error
-    setRegisterError('')
-    setRegisterError('')
+    setRegisterError("");
+    setRegisterError("");
 
     //password validation
     if (password.length < 6) {
-      setRegisterError('Password should be least 6 Characters or longer')
+      setRegisterError("Password should be least 6 Characters or longer");
+      return;
+    } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]/.test(password)) {
+      setRegisterError(
+        "Your password should have at least one upper case,lower case characters and number."
+      );
+      return;
+    } else if (!accepted) {
+      setRegisterError("Please Accept our Terms and Conditions");
       return;
     }
-    
+
     //create user
-    createUserWithEmailAndPassword(auth,email,password)
-    .then(result=>{
-      console.log(result.user);
-      setRegisterSuccess('Your Email Register Successfully')
-    })
-    .catch((error) => {
-       console.log(error);
-       setRegisterError(error.message)
-    });
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        setRegisterSuccess("Your Email Register Successfully");
+
+        // update profile
+        updateProfile(result.user, {
+          displayName: name,
+          photoURL: "https://example.com/jane-q-user/profile.jpg",
+        })
+          .then(() => console.log("profile updated"))
+          .catch();
+
+        //send verification email
+        sendEmailVerification(result.user)
+        .then(() => {
+          alert("Please check your email and Verify your email");
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        setRegisterError(error.message);
+      });
   };
 
   return (
@@ -66,18 +95,57 @@ const Register = () => {
               Email
             </label>
           </div>
-          <div className="relative h-11 w-full ">
+          <div className="relative h-11 w-full gap-1 flex items-center">
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
               name="password"
               required
-              className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+              className="peer relative h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
             />
             <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
               Password
             </label>
+            <span
+              onClick={() => setShowPassword(!showPassword)}
+              className="font-semibold absolute ml-[315px]"
+            >
+              {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+            </span>
           </div>
-          <div className="p-6 pt-0">
+          <div className="-ml-2.5">
+            <div className="inline-flex items-center">
+              <label
+                className="relative flex cursor-pointer items-center rounded-full p-3"
+                htmlFor="terms"
+                data-ripple-dark="true"
+              >
+                <input
+                  type="checkbox"
+                  name="terms"
+                  id="terms"
+                  className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-pink-500 checked:bg-pink-500 checked:before:bg-pink-500 hover:before:opacity-10"
+                />
+                <span className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-3.5 w-3.5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    stroke="currentColor"
+                  >
+                    <path d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"></path>
+                  </svg>
+                </span>
+              </label>
+              <label
+                className="mt-px cursor-pointer select-none font-light text-gray-700"
+                htmlFor="checkbox"
+              >
+                Accept our <a href="">Terms and Conditions</a>
+              </label>
+            </div>
+          </div>
+          <div className="p-2 pt-0">
             <input
               className="block w-full select-none rounded-lg bg-gradient-to-tr from-pink-600 to-pink-400 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
               type="submit"
@@ -86,12 +154,25 @@ const Register = () => {
             />
           </div>
         </form>
-        {
-          registerError && <p className="text-red-600 text-sm font-semibold pb-4 text-center">{registerError}</p>
-        }
-        {
-          registerSuccess && <p className="text-green-600 text-sm font-semibold pb-4 text-center">{registerSuccess}</p>
-        }
+        {registerError && (
+          <p className="text-red-600 text-sm font-semibold pb-4 text-center">
+            {registerError}
+          </p>
+        )}
+        {registerSuccess && (
+          <p className="text-green-600 text-sm font-semibold pb-4 text-center">
+            {registerSuccess}
+          </p>
+        )}
+        <p className="mb-2 flex justify-center font-sans text-sm font-light leading-normal text-inherit antialiased">
+          Already have an account?
+          <Link
+            to={"/login"}
+            className="ml-1 block font-sans text-sm font-bold leading-normal text-pink-500 antialiased"
+          >
+            Login
+          </Link>
+        </p>
       </div>
     </div>
   );
